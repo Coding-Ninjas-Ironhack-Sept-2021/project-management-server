debe22304f134e522345384dcc52bb1fdcaa42bc
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-11 10:56:31 +0100

    functionality for authentication

--- models/Project.model.js
+++ models/Project.model.js
@@ -4,8 +4,8 @@ const { Schema, model } = mongoose;
 const projectSchema = new Schema({
   title: String,
   description: String,
-  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
-  // owner will be added later on
+  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+  owner: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = model('Project', projectSchema);
